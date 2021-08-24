125f3dbcaf8d619339cae60099ecb3b866577d41
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-08-24 10:28:39 +0300

    Added interfaces for repo

---
+++ src/Data/Interfaces/IPositionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data.Entities;
+
+namespace Data.Interfaces
+{
+    public interface IPositionRepository
+    {
+        Task<List<Position>> GetAll();
+    }
+}
