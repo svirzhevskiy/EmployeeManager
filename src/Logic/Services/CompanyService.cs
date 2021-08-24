fcefedb13c957f566d1eacc2776eb0696c7f654b
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-08-24 10:29:56 +0300

    Added services

---
+++ src/Logic/Services/CompanyService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Logic.DTOs;
+using Logic.Interfaces;
+
+namespace Logic.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        public async Task<List<CompanyDTO>> GetAll()
+        {
+            return new List<CompanyDTO>()
+            {
+                new()
+                {
+                    Id = 1, Title = "Epol", LegalForm = new LegalFormDTO() { Id = 1, Title = "OOO"}
+                },
+                new()
+                {
+                    Id = 2, Title = "Epol", LegalForm = new LegalFormDTO() { Id = 1, Title = "OOO"}
+                },
+                new()
+                {
+                    Id = 3, Title = "Epol", LegalForm = new LegalFormDTO() { Id = 1, Title = "OOO"}
+                },
+                new()
+                {
+                    Id = 4, Title = "Epol", LegalForm = new LegalFormDTO() { Id = 1, Title = "OOO"}
+                }
+            };
+        }
+
+        public async Task<CompanyDTO> Create(CompanyDTO dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<CompanyDTO> Update(CompanyDTO dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Logic/Services/EmployeeService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Logic.DTOs;
+using Logic.Interfaces;
+
+namespace Logic.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public async Task<List<EmployeeDTO>> GetAll()
+        {
+            return new List<EmployeeDTO>()
+            {
+                new ()
+                {
+                    Id = 1, Name = "Larry", Surname = "qwe", Company = new CompanyDTO() {Id = 1, Title = "Quilix"},
+                    EmploymentDate = new DateTime(2019, 01, 10), Patronymic = "asd", 
+                    Position =new PositionDTO(){ Id = 1, Title = "qwe"} 
+                },
+                new ()
+                {
+                    Id = 2, Name = "Larry", Surname = "qwe", Company = new CompanyDTO() {Id = 1, Title = "Quilix"},
+                    EmploymentDate = new DateTime(2019, 01, 10), Patronymic = "asd", 
+                    Position =new PositionDTO(){ Id = 1, Title = "qwe"} 
+                },
+                new ()
+                {
+                    Id = 3, Name = "Larry", Surname = "qwe", Company = new CompanyDTO() {Id = 1, Title = "Quilix"},
+                    EmploymentDate = new DateTime(2019, 01, 10), Patronymic = "asd", 
+                    Position =new PositionDTO(){ Id = 1, Title = "qwe"} 
+                },
+                new ()
+                {
+                    Id = 4, Name = "Larry", Surname = "qwe", Company = new CompanyDTO() {Id = 1, Title = "Quilix"},
+                    EmploymentDate = new DateTime(2019, 01, 10), Patronymic = "asd", 
+                    Position =new PositionDTO(){ Id = 1, Title = "qwe"} 
+                },
+                new ()
+                {
+                    Id = 5, Name = "Larry", Surname = "qwe", Company = new CompanyDTO() {Id = 1, Title = "Quilix"},
+                    EmploymentDate = new DateTime(2019, 01, 10), Patronymic = "asd", 
+                    Position =new PositionDTO(){ Id = 1, Title = "qwe"} 
+                },
+            };
+        }
+
+        public async Task<EmployeeDTO> Create(EmployeeDTO dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<EmployeeDTO> Update(EmployeeDTO dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
