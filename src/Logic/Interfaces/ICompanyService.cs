59d4cef614c4c19dc31489f951944e17dfff9296
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-08-25 17:18:40 +0300

    Comments for service interfaces

--- src/Logic/Interfaces/ICompanyService.cs
+++ src/Logic/Interfaces/ICompanyService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Logic.DTOs;
 
@@ -7,11 +6,39 @@ namespace Logic.Interfaces
 {
     public interface ICompanyService
     {
+        /// <summary>
+        /// Get all companies
+        /// </summary>
+        /// <returns>Collection of companies</returns>
         Task<IEnumerable<CompanyDTO>> GetAll();
+        /// <summary>
+        /// Create new company
+        /// </summary>
+        /// <param name="dto">Company data transfer object</param>
+        /// <returns>True if company was successfully added</returns>
         Task<bool> Create(CompanyDTO dto);
+        /// <summary>
+        /// Update data for company
+        /// </summary>
+        /// <param name="dto">Company data transfer object</param>
+        /// <returns>True if company was successfully updated</returns>
         Task<bool> Update(CompanyDTO dto);
+        /// <summary>
+        /// Delete company
+        /// </summary>
+        /// <param name="id">Id of company</param>
+        /// <returns>True if company was successfully deleted</returns>
         Task<bool> Delete(int id);
+        /// <summary>
+        /// Get company by id
+        /// </summary>
+        /// <param name="id">Id of company</param>
+        /// <returns>Company data transfer object</returns>
         Task<CompanyDTO> GetById(int id);
+        /// <summary>
+        /// Get possible legal forms of company 
+        /// </summary>
+        /// <returns>Legal forms for companies</returns>
         Task<IEnumerable<EnumItemDTO>> GetLegalForms();
     }
 }
--- src/Logic/Interfaces/IEmployeeService.cs
+++ src/Logic/Interfaces/IEmployeeService.cs
@@ -6,14 +6,56 @@ namespace Logic.Interfaces
 {
     public interface IEmployeeService
     {
+        /// <summary>
+        /// Get all employees
+        /// </summary>
+        /// <returns>Collection of employees</returns>
         Task<IEnumerable<EmployeeDTO>> GetAll();
+        /// <summary>
+        /// Get employees for specified page
+        /// </summary>
+        /// <param name="page">Page number</param>
+        /// <param name="itemsPerPage">Number of employees on page</param>
+        /// <returns>Collection of employees</returns>
         Task<IEnumerable<EmployeeDTO>> GetAll(int page, int itemsPerPage);
+        /// <summary>
+        /// Create new employee
+        /// </summary>
+        /// <param name="dto">Employee data transfer object</param>
+        /// <returns>True if employee was successfully created</returns>
         Task<bool> Create(EmployeeDTO dto);
+        /// <summary>
+        /// Update data for employee
+        /// </summary>
+        /// <param name="dto">Employee data transfer object</param>
+        /// <returns>True if employee was successfully updated</returns>
         Task<bool> Update(EmployeeDTO dto);
+        /// <summary>
+        /// Delete employee
+        /// </summary>
+        /// <param name="id">Id of employee</param>
+        /// <returns>True if employee was successfully deleted</returns>
         Task<bool> Delete(int id);
+        /// <summary>
+        /// Get employee with specified id
+        /// </summary>
+        /// <param name="id">Id of employee</param>
+        /// <returns>Employee data transfer object</returns>
         Task<EmployeeDTO> GetById(int id);
+        /// <summary>
+        /// Get possible positions for employee
+        /// </summary>
+        /// <returns>Collection of employee positions</returns>
         Task<IEnumerable<EnumItemDTO>> GetPositions();
+        /// <summary>
+        /// Get companies
+        /// </summary>
+        /// <returns>Collection of companies</returns>
         Task<IEnumerable<EnumItemDTO>> GetCompanies();
+        /// <summary>
+        /// Get total number of employees
+        /// </summary>
+        /// <returns>Number of employees</returns>
         Task<int> CountEmployees();
     }
 }
