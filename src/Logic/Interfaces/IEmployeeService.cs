9d5f6c65960122ec3bc3764fc6e32ec395231d1f
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-08-25 16:07:57 +0300

    Added paging functions to EmployeeService

--- src/Logic/Interfaces/IEmployeeService.cs
+++ src/Logic/Interfaces/IEmployeeService.cs
@@ -7,11 +7,13 @@ namespace Logic.Interfaces
     public interface IEmployeeService
     {
         Task<IEnumerable<EmployeeDTO>> GetAll();
+        Task<IEnumerable<EmployeeDTO>> GetAll(int page, int itemsPerPage);
         Task<bool> Create(EmployeeDTO dto);
         Task<bool> Update(EmployeeDTO dto);
         Task<bool> Delete(int id);
         Task<EmployeeDTO> GetById(int id);
         Task<IEnumerable<EnumItemDTO>> GetPositions();
         Task<IEnumerable<EnumItemDTO>> GetCompanies();
+        Task<int> CountEmployees();
     }
 }
