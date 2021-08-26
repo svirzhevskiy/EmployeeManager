798f40892aa2ced1ac25dfc259b6c2343ff4a401
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-08-26 12:54:39 +0300

    Removed unnecessary usings

--- src/Logic/Services/EmployeeService.cs
+++ src/Logic/Services/EmployeeService.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Data.Entities;
 using Data.Interfaces;
 using Logic.DTOs;
 using Logic.Interfaces;
